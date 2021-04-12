5daff5da8b05e104127a6a045b9dbda7add0d28d
Jess <jessylayton@yahoo.co.uk>
2021-04-12 09:33:09 +0100

    fe initial

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'react-app',
+    'airbnb',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'jsx-a11y',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'max-len': ['error', { code: 180 }],
+    'no-param-reassign': [2, { props: false }],
+    'jsx-quotes': [2, 'prefer-single'],
+    'consistent-return': 'off',
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'no-console': 'off',
+  },
+};

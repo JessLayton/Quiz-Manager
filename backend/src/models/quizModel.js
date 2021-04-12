0a3179958cb442f4932a4bf74401342ebd373743
Jess <jessylayton@yahoo.co.uk>
2021-04-12 09:29:21 +0100

    models

---
+++ backend/src/models/quizModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { quizDb } = require('../dbs');
+
+const questionSchema = new mongoose.Schema({
+  question: {
+    type: String, required: true, maxLength: 80,
+  },
+  options: {
+    type: [String],
+    required: true,
+    validate: {
+      validator: (options) => options.length === 4,
+      message: () => 'Questions must have 4 options',
+    },
+  },
+  correctOption: {
+    type: Number, default: 1, required: true,
+  },
+});
+
+const quizSchema = new mongoose.Schema({
+  name: {
+    type: String, required: true, maxLength: 50, unique: true,
+  },
+  creator: { type: String },
+  description: { type: String, required: true, maxLength: 100 },
+  questions: { type: [questionSchema], required: true, minlength: 1 },
+});
+
+const Quiz = quizDb.model('quiz', quizSchema);
+
+module.exports = Quiz;
---
+++ backend/src/models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { userDb } = require('../dbs');
+
+const emailCheck = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordCheck = /^((?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W).{8,})$/;
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: (isEmail) => emailCheck.test(isEmail),
+      message: (props) => `${props.value} is not a valid email`,
+    },
+    required: [true, 'Email is required field'],
+  },
+  password: {
+    type: String,
+    validate: {
+      validator: (isPassword) => passwordCheck.test(isPassword),
+      message: 'Invalid password - password must be 8 characters including uppercase, special and alphanumeric characters',
+    },
+    required: [true, 'Password is required field'],
+    minlength: 8,
+    maxLength: 100,
+  },
+  username: { type: String, maxLength: 25, minlength: 5 },
+  role: {
+    type: String,
+    default: 'user',
+  },
+});
+
+const User = userDb.model('user', userSchema);
+
+module.exports = User;

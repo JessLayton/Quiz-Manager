abf5a4779c387b99e83746b7ae58d51d0fcc33bf
Jess <jessylayton@yahoo.co.uk>
2021-04-16 09:32:55 +0100

    updated fe test

--- frontend/src/_tests_/CreateQuiz.test.js
+++ frontend/src/_tests_/CreateQuiz.test.js
@@ -6,7 +6,6 @@ import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import CreateQuiz from '../components/quiz/CreateQuiz';
-import NavBar from '../components/NavBar';
 
 import QuizForm from '../components/quiz/quizForm/QuizForm';
 import store from '../store';
@@ -14,13 +13,12 @@ import store from '../store';
 configure({ adapter: new Adapter() });
 
 describe('<CreateQuiz />', () => {
-  it('contains a <Navbar /> and a <QuizForm /> component', () => {
+  it('contains a <QuizForm /> component', () => {
     const wrapper = mount((
       <Provider store={store}>
         <CreateQuiz />
       </Provider>
     ));
     expect(wrapper.exists(QuizForm)).to.be.true;
-    expect(wrapper.exists(NavBar)).to.be.true;
   });
 });
